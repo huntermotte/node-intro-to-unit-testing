93b42e40bc03989139280aa65bab80f06f6d7f3c
Hunter Motte <huntermotte@gmail.com>
2017-04-27 23:13:01 -0400

    initial commit

---
+++ test/test-fizzBuzzer.js
@@ -0,0 +1,47 @@
+const should = require('chai').should();
+const adder = require('../fizzBuzzer');
+
+describe('fizzBuzzer', function() {
+  it('should return fizzBuzz if the number is divisible by 15', function() {
+    const normalCases = [15,30,45];
+    normalCases.forEach(input) {
+      fizzBuzzer(input).should.equal('fizz-buzz');
+    }
+  })
+});
+
+describe('fizzBuzzer', function () {
+  it('should return buzz if the number is divisible by 5', function() {
+    const normalCases = [5,10,20];
+    normalCases.forEach(input) {
+      fizzBuzzer(input).should.equal('buzz')
+    }
+  }
+});
+
+describe('fizzBuzzer', function () {
+  it('should return fizz is the number is divisible by 3', function () {
+    const normalCases = [3,6,9];
+    normalCases.forEach(input) {
+      fizzBuzzer(input).should.equal('fizz')
+    }
+  })
+});
+
+describe('fizzBuzzer', function() {
+  it('should return a number if not a fizzBuzz number', function() {
+    const normalCases = [1,2,4,7,11];
+    normalCases.forEach(input) {
+      fizzBuzzer(input).should.equal(input);
+    }
+  })
+});
+
+describe('fizzBuzzer', function() {
+  it('should give us an error if not a number', function() {
+    const errorCases = ['a', true, function()];
+    errorCases.forEach(function(input) {
+      fizzBuzzer(input).should.throw(Error)
+    }
+  })
+});
